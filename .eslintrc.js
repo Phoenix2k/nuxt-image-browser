99c66925fa74b9e7d8237b91c4bc6db9f90b5903
Niko Pettersen <niko.pettersen@gmail.com>
2018-10-16 23:49:02 +0300

    Add personal formatting preferences

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,85 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    'plugin:vue/recommended',
-    'plugin:prettier/recommended'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue',
-    'prettier'
-  ],
-  // add your custom rules here
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+	env: {
+		browser: true,
+		node: true
+	},
+	extends: [ 'plugin:vue/recommended' ],
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	plugins: [ 'vue' ],
+	root: true,
+	rules: {
+		'array-bracket-newline': [
+			'error', {
+				minItems: 4,
+				multiline: true
+			}
+		],
+		'array-bracket-spacing': [
+			'error', 'always', {
+				arraysInArrays: false,
+				objectsInArrays: false
+			}
+		],
+		'array-in-arrays': [
+			'always', {
+				arraysInArrays: false
+			}
+		],
+		'arrays-in-objects': 'always',
+		'comma-dangle': [ 'error', 'never' ],
+		'computed-property-spacing': [ 'error', 'always' ],
+		'indent': [ 'error', 'tab' ],
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-extra-semi': 'error',
+		'no-tabs': [
+			'error', {
+				allowIndentationTabs: true
+			}
+		],
+		'object-curly-spacing': [
+			'error', 'always', {
+				arraysInObjects: true,
+				objectsInObjects: false
+			}
+		],
+		'objects-in-arrays': 'always',
+		'quote-props': [ 'error','consistent-as-needed' ],
+		'quotes': [ 'error', 'single' ],
+		'semi': [ 'error', 'always' ],
+		'space-in-parens': [ 'error', 'always' ],
+		'vue/html-closing-bracket-newline': [
+			'error', {
+				multiline: 'never',
+				singleline: 'never'
+			}
+		],
+		'vue/html-closing-bracket-spacing': [
+			'error', {
+				endTag: 'never',
+				selfClosingTag: 'always',
+				startTag: 'never'
+			}
+		],
+		'vue/html-indent': [
+			'error', 'tab', {
+				alignAttributesVertically: false,
+				attribute: 1,
+				closeBracket: 0,
+				ignores: []
+			}
+		],
+		'vue/max-attributes-per-line': [
+			'error', {
+				multiline: {
+					allowFirstLine: false,
+					max: 1
+				},
+				singleline: 6
+			}
+		],
+		'vue/script-indent': [ 'error', 'tab' ]
+	}
+};
